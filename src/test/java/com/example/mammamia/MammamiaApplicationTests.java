59eb52ac9ad235c5a2860583e5c55eba085727b3
cmylmzee <74065297+cmylmzee@users.noreply.github.com>
2022-06-28 14:22:59 +0300

    ilk repo deneme

---
+++ src/test/java/com/example/mammamia/MammamiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mammamia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MammamiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
